ec6f69b43261b10e09c66dfeabbb56b6b728d8e9
Daniele-sc <72503335+Daniele-sc@users.noreply.github.com>
2020-11-19 19:07:04 +0100

    Update GitHub_trial.R
    Ho aggiunto al modello un nuovo repressore.

--- GitHub_trial.R
+++ GitHub_trial.R
@@ -4,5 +4,5 @@ library(ggplot2)
 ggplot(iris, aes(x = Sepal.Length, y = Sepal.Width)) + 
   geom_point()
 
-mod <- lm(Sepal.Length ~ Sepal.Width, iris)
+mod <- lm(Sepal.Length ~ Sepal.Width + Petal.Length, iris)
 summary(mod)
